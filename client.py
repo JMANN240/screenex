4bf258ac5b73a2f89ccbdad3e6afefa931699e9d
JT Raber <jmann240raber@gmail.com>
2022-01-14 23:41:22 -0500

    sockets

--- client.py
+++ client.py
@@ -1,16 +1,55 @@
-import socketio
-import pyautogui
+import socket
 from pynput.mouse import Controller as MouseController
+from pynput.mouse import Listener as MouseListener
 
-screensize = pyautogui.size()
 mouseController = MouseController()
 
-sio = socketio.Client()
+init_pos = tuple(mouseController.position)
+pos = list(mouseController.position)
 
-@sio.on('mouse')
-def handle_mouse(x, y):
-    print("mouse event")
-    print(x, y)
-    # mouseController.position = (x, screensize+y)
+def move_callback(x, y):
+    global pos, init_pos, suppressedMouseListener, mouseListener
+    pos = [x, y]
+    print(f"not:{pos}")
+    if mouseListener.running and pos[1] < 0:
+        print("switching to suppressing")
+        mouseListener.stop()
+        init_pos = (mouseController.position[0], 0)
+        suppressedMouseListener = MouseListener(on_move=suppressed_move_callback, suppress=True)
+        suppressedMouseListener.start()
+        suppressedMouseListener.wait()
 
-sio.connect('http://10.42.0.226:8070')
+def suppressed_move_callback(x, y):
+    global pos, init_pos, suppressedMouseListener, mouseListener
+    pos[0] += x - init_pos[0]
+    pos[1] += y - init_pos[1]
+    print(f"sup:{pos}")
+    if suppressedMouseListener.running and pos[1] >= 0:
+        print("switching to free")
+        suppressedMouseListener.stop()
+        mouseController.position = tuple(pos)
+        mouseListener = MouseListener(on_move=move_callback)
+        mouseListener.start()
+        mouseListener.wait()
+
+mouseListener = MouseListener(on_move=move_callback)
+suppressedMouseListener = MouseListener(on_move=suppressed_move_callback, suppress=True)
+
+mouseListener.start()
+mouseListener.wait()
+
+server = ('10.42.0.1', 4000)
+client = ('10.42.0.226', 4001)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(client)
+
+last_pos = list(pos)
+
+while pos != [100, 100]:
+    if pos != last_pos:
+        s.sendto(f"{pos[0]} {pos[1]}".encode('utf-8'), server)
+        last_pos = list(pos)
+
+s.sendto("quit".encode('utf-8'), server)
+s.close()
--- server.py
+++ server.py
@@ -1,53 +1,22 @@
-from pynput.keyboard import Listener as KeyListener
+import socket
+import pyautogui
 from pynput.mouse import Controller as MouseController
-from pynput.mouse import Listener as MouseListener
-from time import sleep, time
-import socketio
 
-sio = socketio.Server()
-
+screensize = pyautogui.size()
 mouseController = MouseController()
 
-init_pos = tuple(mouseController.position)
-pos = list(mouseController.position)
+server = ('10.42.0.1', 4000)
+client = ('10.42.0.226', 4001)
 
-def move_callback(x, y):
-    global pos
-    pos = [x, y]
-    print(f"not:{pos}")
-
-def suppressed_move_callback(x, y):
-    global pos, init_pos
-    pos[0] += x - init_pos[0]
-    pos[1] += y - init_pos[1]
-    print(f"sup:{pos}")
-    sio.emit('mouse', {'x': x, 'y': y})
-
-mouseListener = MouseListener(on_move=move_callback)
-suppressedMouseListener = MouseListener(on_move=suppressed_move_callback, suppress=True)
-
-mouseListener.start()
-mouseListener.wait()
-
-t = time()
-
-while time() - t < 60:
-    if (mouseListener.running and pos[1] < 0):
-        print("switching to suppressing")
-        mouseListener.stop()
-        init_pos = tuple(mouseController.position)
-        suppressedMouseListener = MouseListener(on_move=suppressed_move_callback, suppress=True)
-        suppressedMouseListener.start()
-        suppressedMouseListener.wait()
-    
-    
-    if (suppressedMouseListener.running and pos[1] >= 0):
-        print("switching to free")
-        suppressedMouseListener.stop()
-        mouseController.position = tuple(pos)
-        mouseListener = MouseListener(on_move=move_callback)
-        mouseListener.start()
-        mouseListener.wait()
-
-mouseListener.stop()
-suppressedMouseListener.stop()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(server)
+data = ""
+while True:
+    data, addr = s.recvfrom(1024)
+    data = data.decode('utf-8')
+    if data == "quit":
+        break
+    coords = (int(coord) for coord in data.split(" "))
+    x = coords[0]
+    y = coords[1]
+    mouseController.position = (x, screensize+y)
