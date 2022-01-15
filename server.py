e13a754c21983ff7888e09188ac4735dad56a63c
JT Raber <jmann240raber@gmail.com>
2022-01-15 00:53:23 -0500

    better messages

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 import socket
 import pyautogui
 from pynput.mouse import Controller as MouseController
+from pynput.keyboard import Controller as KeyboardController
+from pynput import KeyCode
+import json
 
 screensize = pyautogui.size()
 mouseController = MouseController()
+keyboardController = KeyboardController()
 
 server = ('10.42.0.1', 4000)
 client = ('10.42.0.226', 4001)
@@ -14,9 +18,16 @@ data = ""
 while True:
     data, addr = s.recvfrom(1024)
     data = data.decode('utf-8')
-    if data == "quit":
+    data = json.loads(data)
+    print(json.dumps(data, indent=4))
+    if data["message"] == "mouse":
+        x = data["coords"]["x"]
+        y = data["coords"]["y"]
+        mouseController.position = (x, screensize+y)
+    elif data["message"] == "press":
+        keycode = data["keycode"]
+    elif data["message"] == "unpress":
+        keycode = data["keycode"]
+        # keyboardController.tap(KeyCode.from_vk(keycode))
+    elif data["message"] == "quit":
         break
-    coords = (int(coord) for coord in list(data.split(" ")))
-    x = coords[0]
-    y = coords[1]
-    mouseController.position = (x, screensize+y)
