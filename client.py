08ca24c6d79fa429b8a4e0e2dd95739fdf0cc3f8
JT Raber <jmann240raber@gmail.com>
2022-01-14 20:55:23 -0500

    fixed client

--- client.py
+++ client.py
@@ -9,6 +9,6 @@ sio = socketio.Client()
 
 @sio.on('mouse')
 def handle_mouse(x, y):
-    mouseController.position = (x, y)
+    mouseController.position = (x, screensize+y)
 
 sio.connect('http://10.244.34.14:8070')
